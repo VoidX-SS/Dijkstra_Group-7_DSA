f8bc5afa3227f03cc50deac518a550a54fb02efe
VoidX-SS <143262463+VoidX-SS@users.noreply.github.com>
2026-04-15 02:16:12 +0700

    Update CustomLinkedList.cs

--- DataStructures/CustomLinkedList.cs
+++ DataStructures/CustomLinkedList.cs
@@ -4,14 +4,14 @@ namespace DoAnCuoiKy_Dijkstra
 {
     public class CustomLinkedList<T>
     {
-        public ListNode<T> flink { get; private set; }
-        public ListNode<T> blink { get; private set; }
+        public ListNode<T> Head { get; private set; }
+        public ListNode<T> Tail { get; private set; }
         public int Count { get; private set; }
 
-        public MyLinkedList()
+        public CustomLinkedList()
         {
-            flink = null;
-            blink = null;
+            Head = null;
+            Tail = null;
             Count = 0;
         }
 
@@ -20,15 +20,15 @@ namespace DoAnCuoiKy_Dijkstra
         {
             ListNode<T> newNode = new ListNode<T>(data);
 
-            if (flink == null)
+            if (Head == null)
             {
-                flink = blink = newNode;
+                Head = Tail = newNode;
             }
             else
             {
-                blink.Next = newNode;
-                newNode.Prev = blink;
-                blink = newNode;
+                Tail.Next = newNode;
+                newNode.Prev = Tail;
+                Tail = newNode;
             }
             Count++;
         }
@@ -39,7 +39,7 @@ namespace DoAnCuoiKy_Dijkstra
             if (index < 0 || index >= Count)
                 throw new IndexOutOfRangeException("Vị trí không hợp lệ.");
 
-            ListNode<T> current = flink;
+            ListNode<T> current = Head;
             for (int i = 0; i < index; i++)
             {
                 current = current.Next;
@@ -50,20 +50,20 @@ namespace DoAnCuoiKy_Dijkstra
         // Hàm Remove
         public void Remove(ListNode<T> nodeToRemove)
         {
-            if (nodeToRemove == null || flink == null) return;
+            if (nodeToRemove == null || Head == null) return;
 
-            // Nếu là flink
-            if (nodeToRemove == flink)
+            // Nếu là Head
+            if (nodeToRemove == Head)
             {
-                flink = nodeToRemove.Next;
-                if (flink != null) flink.Prev = null;
-                else blink = null; // List có 1 phần tử
+                Head = nodeToRemove.Next;
+                if (Head != null) Head.Prev = null;
+                else Tail = null; // List có 1 phần tử
             }
-            // Nếu là blink
-            else if (nodeToRemove == blink)
+            // Nếu là Tail
+            else if (nodeToRemove == Tail)
             {
-                blink = nodeToRemove.Prev;
-                if (blink != null) blink.Next = null;
+                Tail = nodeToRemove.Prev;
+                if (Tail != null) Tail.Next = null;
             }
             // Nếu nằm giữa
             else
